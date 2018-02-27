e10ba7275e29b013f142a6d05632b05cc9bcdabb
Paulo Marques <paulomarquesc@users.noreply.github.com>
2018-02-27 15:36:04 -0700

    Initial testing commit

---
+++ HOL/ARM-NewIaaS/scripts/StartConfigureSQL.ps1
@@ -0,0 +1,18 @@
+param
+(
+	[string]$adminUsername,
+	[string]$adminPassword
+)
+
+Start-Transcript -path "$PSScriptRoot\StartConfigureSQLLog.txt"
+
+$password =  ConvertTo-SecureString $adminPassword -AsPlainText -Force
+[System.Management.Automation.PSCredential]$credential = New-Object System.Management.Automation.PSCredential ("$env:COMPUTERNAME\$adminUserName", $password)
+
+$command = "$PSScriptRoot\ConfigureSQL.ps1"
+Enable-PSRemoting –force -ErrorAction SilentlyContinue
+Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
+#Disable-PSRemoting -Force  -ErrorAction SilentlyContinue
+
+Stop-Transcript
+[string]::Join("`r`n",(Get-Content "$PSScriptRoot\StartConfigureSQLLog.txt")) | Out-File "$PSScriptRoot\StartConfigureSQLLog.txt"
