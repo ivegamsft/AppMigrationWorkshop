72c1f0770e1be63b860188f27d723694743a2f36
ivega@microsoft.com <ivega@microsoft.com>
2018-03-19 19:36:55 -0400

    Fixed bad char

--- Shared/ARM-NewIaaS/scripts/StartConfigureSQL.ps1
+++ Shared/ARM-NewIaaS/scripts/StartConfigureSQL.ps1
@@ -10,7 +10,7 @@ $password =  ConvertTo-SecureString $adminPassword -AsPlainText -Force
 [System.Management.Automation.PSCredential]$credential = New-Object System.Management.Automation.PSCredential ("$env:COMPUTERNAME\$adminUserName", $password)
 
 $command = "$PSScriptRoot\ConfigureSQL.ps1"
-Enable-PSRemoting –force -ErrorAction SilentlyContinue
+Enable-PSRemoting -force -ErrorAction SilentlyContinue
 Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
 #Disable-PSRemoting -Force  -ErrorAction SilentlyContinue
 
