71e9e8c16e753787dd4612efe72a773fb6411baa
Jennelle Crothers <jkcrothers@hotmail.com>
2021-07-20 13:18:07 -0700

    lb and pip to standard

--- Shared/TF-NewIaaS/modules/loadbalancer/lb.tf
+++ Shared/TF-NewIaaS/modules/loadbalancer/lb.tf
@@ -3,12 +3,14 @@ resource "azurerm_public_ip" "ext-lb" {
   location            = var.location
   resource_group_name  = var.resource_group_name
   allocation_method   = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_lb" "ext-lb" {
   name                = var.lb_name
   location            = var.location
   resource_group_name  = var.resource_group_name
+  sku = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
